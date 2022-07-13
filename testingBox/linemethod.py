33eee12a217dac46c4e26525fc2b4c5de310b078
chihunghwang_asusTUP_tiri <chihunghwang@gmail.com>
2022-07-13 17:18:32 +0800

    test

--- testingBox/linemethod.py
+++ testingBox/linemethod.py
@@ -42,7 +42,6 @@ class testing:
             ws[group_in_5] = int(i+target_min-2)
             # print(target_box)
             # print(ws[target_box].value)  #---> float(when single box)
-        
         wb.save(self.name)
 
     def test2(self):
@@ -64,6 +63,31 @@ class testing:
         wb.save(self.name)
 
 
-testing('./testingBox/pt_diff_0_1_th_16_1_th.xlsx').test2()
+    def test3(self):
+        file_home = self.name
+        wb = load_workbook(filename=file_home)
+        ws = wb['data']
+        
+        for i in range (180+2,180+2150-2-1):
+            target_box_0 = 'G'+str(i)
+            target_box_1 = 'G'+str(i+1)
+            target_box_2 = 'G'+str(i+2)
+            target_box_3 = 'G'+str(i+3)
+            target_box_4 = 'G'+str(i+4)
+            # print(ws[target_box_0].value)
+            # print(type(ws[target_box_0].value))
+            list_target_box_temp=[ws[target_box_0].value,ws[target_box_1].value,ws[target_box_2].value,ws[target_box_3].value,ws[target_box_4].value]
+            target_min=list_target_box_temp.index(min(list_target_box_temp))
+            print(i+target_min)
+            group_in_5 = 'G'+str(i)
+            ws[group_in_5] = int(i+target_min-2)
+            # print(target_box)
+            # print(ws[target_box].value)  #---> float(when single box)
+        wb.save(self.name)
+
+
+
+
+# testing('./testingBox/pt_diff_0_1_th_16_1_th.xlsx').test3()
 
 
