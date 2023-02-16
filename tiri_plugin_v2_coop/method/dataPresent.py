ba38fe5163cb7556005040515c4ee114a3c21f5c
RongQing Qiu <yauwhing@gmail.com>
2023-02-16 23:47:57 +0800

    update (time reduction)and debug dataPresent.py

--- tiri_plugin_v2_coop/method/dataPresent.py
+++ tiri_plugin_v2_coop/method/dataPresent.py
@@ -16,15 +16,32 @@ def create_xlsx(path4save):
     print('Done create_xlsx')
 
 
-def data_fill_in(path_xlsx2edit,row2start,col2start,case,ls_data):
+def data_fill_in(path_xlsx2edit,ls_data):
     wb=load_workbook(path_xlsx2edit)
     sheet_editing = wb['DATA']
-    sheet_editing.cell(row=1,column =col2start,value=str(case))
-    for frame in range (0,len(ls_data)-14): # 14 = -1 -13(txt file data starts from #13) 
-        sheet_editing.cell(row=int(row2start)+2+frame,column=col2start,value = ls_data[frame+13])
+
+    all_col = 1
+    for term_case in ls_data:
+        
+        #obtain starting frame
+        area_start_frame=int(term_case[0])
+        case_name = str(term_case[1])
+        sheet_editing.cell(row=1,column =all_col,value=case_name)
+        for data2fill in range (0,int(len(term_case)-15)):
+            sheet_editing.cell(row=int(data2fill+2+area_start_frame),column =all_col,value=term_case[data2fill+15])    # the actual area common starting frame is excel_row-2
+        
+        all_col+=1
+    
     wb.save(path_xlsx2edit)
     
-    print('Done data_fill_in '+str(case))
+    # #
+
+    # sheet_editing.cell(row=1,column =col2start,value=str(case))
+    # for frame in range (0,len(ls_data)-14): # 14 = -1 -13(txt file data starts from #13) 
+    #     sheet_editing.cell(row=int(row2start)+2+frame,column=col2start,value = ls_data[frame+13])
+    # wb.save(path_xlsx2edit)
+    
+    # print('Done data_fill_in '+str(case))
 
 
 
@@ -34,11 +51,13 @@ def get_data_from_txt(path_data_folder):
     path4glob = path_data_folder+'/*.txt'
     ls_txt = glob.glob(path4glob)
 
-    txt_counter = 1 # setting it start at 1 (xlsx counter begins from 1 )
+    # txt_counter = 1 # setting it start at 1 (xlsx counter begins from 1 )
 
     path_xlsx_output = path_data_folder+'/'+str(path_data_folder.split('/')[-1])+'.xlsx'
 
     create_xlsx(path_xlsx_output)
+
+    ls_allTxt = []
     for txt in ls_txt:
         
         ls_line=[]
@@ -46,34 +65,52 @@ def get_data_from_txt(path_data_folder):
         for line in f.readlines():
             line=line.replace('\n','')
             ls_line.append(line)
-        f.close()
+        f.close() 
 
         area_starting_frame=int(str(ls_line[10]).split(':')[1])
         case_name = txt.split('/')[-1]
         case_name = str(case_name).replace('.txt','')
 
-        data_fill_in(str(path_xlsx_output),area_starting_frame,txt_counter,case_name,ls_line)
+        ls_line.insert(0,area_starting_frame)
+        ls_line.insert(1,case_name)
+        # !!! ls_line[int(area_starting_frame,str(case_name),.......)] <---- structure
 
-        txt_counter+=1
+        ls_allTxt.append(ls_line)
 
-        print('Done get_data_from_txt '+str(path_data_folder))
+
+        # do data fill in to the xlsx
+    data_fill_in(path_xlsx_output,ls_allTxt)
+    print('Done get_data_from_txt '+str(path_data_folder))
 
 
 def splitting_data(path_data_folder):
     path4glob = path_data_folder+'/*.txt'
     ls_txt = glob.glob(path4glob)
-    len_folder = len(ls_txt)/1000
-    if len(ls_txt)%1000>0:
-        len_folder=len_folder+1
+
+    len_folder=0
+    if len(ls_txt)>=1000:
+        len_folder = len(ls_txt)/1000
+        if len_folder < 1:
+            len_folder =1
+        if len(ls_txt)%1000>0:
+            len_folder=len_folder+1
+    if len(ls_txt) < 1000:
+        len_folder =1
     
+    # create sub-folders
     ls_subfolder = []
-    for folder_num in range (0,len_folder-1):
-        cmd_line = path_data_folder+'/DATA_'+str(folder_num)
-        os.system('mkdir '+cmd_line)
-        ls_subfolder.append(cmd_line)
+    if len_folder > 1: 
+        for folder_num in range (0,int(len_folder)):
+            cmd_line = path_data_folder+'/DATA_'+str(folder_num)
+            os.system('mkdir '+cmd_line)
+            ls_subfolder.append(cmd_line)
+    if len_folder == 1:
+        os.system('mkdir '+str(path_data_folder)+'/DATA_0')
+        ls_subfolder= [str(path_data_folder)+'/DATA_0']
     
+ 
     sub_folder_num2use = 0
-    for ii in range (0,len(ls_txt)-1):
+    for ii in range (0,len(ls_txt)):
 
         shutil.move(ls_txt[ii],ls_subfolder[sub_folder_num2use])
         if ii != 0:
--- tiri_plugin_v2_coop/testingBox/to_theta.py
+++ tiri_plugin_v2_coop/testingBox/to_theta.py
@@ -15,4 +15,15 @@ def a():
     rounded = round(number/10)*10
     print('Rounded Number :', rounded)
 
-a()
+def ins():
+    lks = [1,2,3,4]
+    lks.insert(0,0)
+    print(lks)
+
+def ssj():
+    lk=[1,2,3,4,5,6,7,8,9,10]
+    print(len(lk))
+    for i in range (2,10):
+        print(lk[i])
+
+ssj()
