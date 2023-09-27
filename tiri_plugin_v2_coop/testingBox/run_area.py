22104ae807fd607e2600494b143045a04bef44bf
RongQing Qiu <73519224+riverskawa@users.noreply.github.com>
2023-09-27 14:03:16 +0800

    Delete tiri_plugin_v2_coop/testingBox directory

--- tiri_plugin_v2_coop/testingBox/run_area.py
+++
@@ -1,9 +0,0 @@
-
-
-
-def a_threshold_test():
-    import testingBox.transfer_coordinate
-    import testingBox.to_theta
-    
-    
-    list_coordinate=testingBox.transfer_coordinate.test('./testingBox/testingData_a_threshold_normalCase_1_0_th.txt')
